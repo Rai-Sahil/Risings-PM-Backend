c57b74f160d41e8949fde6bcddff5d6c539dfd8c
Sahil Rai <78111534+Rai-Sahil@users.noreply.github.com>
2024-07-04 17:44:57 -0700

    Update app.go

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -12,7 +12,7 @@ func main() {
 	router := mux.NewRouter()
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173", "https://pm-frontend-swart.vercel.app", "http://54.218.199.46"},
+		AllowedOrigins:   []string{"http://localhost:5173", "https://pm-frontend-swart.vercel.app", "http://54.218.199.46", "https://pm-frontend.netlify.app"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
