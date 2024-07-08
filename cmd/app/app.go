af2621c41b3ee0a2ede88f21bd76d5575606ec53
Sahil Rai <raisahil580@gmail.com>
2024-07-08 14:08:35 -0700

    sdf

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -12,7 +12,7 @@ func main() {
 	router := mux.NewRouter()
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://pm-frontend-swart.vercel.app", "http://localhost:5173"},
+		AllowedOrigins:   []string{"https://pm-frontend-swart.vercel.app", "http://localhost:5173", "http://localhost:80"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Authorization", "Content-Type"},
 		AllowCredentials: true,
